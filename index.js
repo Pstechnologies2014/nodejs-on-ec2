6d56eb0da5d60666afe9dfe54c10fceababc5ef9
Pstechnologies2014 <126408840+Pstechnologies2014@users.noreply.github.com>
2023-12-30 15:16:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
+  res.write('WELCOME TO PS TECHNOLOGIES DEPLOYED IN THE NODEJS APPLICATION USING EC2 INSTANCE'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
